94515793ffb2aea30498b293d19c32a46202088d
yash bansod <yashbansodgit@gmail.com>
2024-03-25 18:25:37 +0530

    feat: authorization setup

--- src/category/router.ts
+++ src/category/router.ts
@@ -5,6 +5,9 @@ import { categoryValidator } from "./validator";
 import { logger } from "../configs/logger";
 import { CategoryModel } from "./model";
 import { asyncWrapper } from "../common/lib/async-wrapper";
+import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/can-access";
+import { UserRoles } from "../common/constants";
 
 const router = express.Router();
 
@@ -13,6 +16,8 @@ const categoryController = new CategoryController(categoryService, logger);
 
 router.post(
     "/",
+    authenticate,
+    canAccess([UserRoles.ADMIN]),
     categoryValidator,
     asyncWrapper(categoryController.create.bind(categoryController)),
 );
